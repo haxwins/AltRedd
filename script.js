c79d9728b48d606ad5341e6662df39b912b29800
haxwins <haxregist@gmail.com>
2017-05-29 20:07:51 +0200

    add

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
+var url = prompt("");
+url = "https://www.reddit.com/r/" + url + ".json";
+console.log(url);
 
+function jsonLoad(url){
+	var req = new XMLHttpRequest();
+	console.log(req.onreadystatechange);
+	console.log(req.readyState);
+	req.open("GET", url, true);
+	req.addEventListener("load", () => {
+		console.log(req.status);
+		if(req.status == 200 && req.readyState == 4){
+			var file = JSON.parse(req.responseText);
+			console.log(file);
+			setBoard(file);
+			return s;
+		}
+		else{
+			console.log("errr");
+		}
+	});
+	req.send(null);
+}
+function setBoard(file){
+	console.log(file);
+	console.log("lmao");
+	file = file.data.children;
+	console.log(file);
+}
+jsonLoad(url);
 	
